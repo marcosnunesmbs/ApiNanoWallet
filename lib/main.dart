6fcea551c6272a8c8bd2d7f4ca1ec8a02ea9e629
Marcos Nunes <marcosnunesmbs@gmail.com>
2019-10-07 17:37:30 -0300

    change primary color

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Dinheiro Contado',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.indigo[700],
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Dinheiro Contado'),
     );
   }
 }
